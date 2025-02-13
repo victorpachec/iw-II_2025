3d543d30a8d804f20d78474d55cd9b2889b38a65
victorpachec <161628721+victorpachec@users.noreply.github.com>
2025-02-13 18:22:37 -0300

    Add files via upload

---
+++ aula 02/exemplo01.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>vitor</title>
+</head>
+<body>
+    <p>oi 2f sou o vitor</p>
+    <?php
+    $nome = "vitor";
+    echo "<p> oi 2f sou o vitor</p>";
+    echo "<p>oi 2f eu sou o $nome </p>";
+    echo "<p>oi 2f eu sou o ".$nome." </p>";
+
+    ?>
+</body>
+</html>
---
+++ aula 02/exemplo02.php
@@ -0,0 +1,8 @@
+<?php
+    $a = 4;
+    $b = "dalmadas 101 ";
+    $s = $a + $b;
+
+    echo $s;
+
+?>
